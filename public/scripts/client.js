d4eba127d6981d3fb96509588029d7f746d081c8
Catherine J <cjarocki@gmail.com>
2017-03-09 13:40:08 -0600

    routes and client.js

---
+++ public/scripts/client.js
@@ -0,0 +1,23 @@
+var myApp = angular.module('warehouseApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/warehouse', {
+      templateUrl: '/views/warehouse.html',
+      controller: 'WarehouseController',
+      controllerAs: 'wc'
+    })
+    .when('/customers', {
+      templateUrl: '/views/customers.html',
+      controller: 'CustomerController',
+      controllerAs: 'cc'
+    })
+    .when('/orders', {
+      templateUrl: '/views/orders.html',
+      controller: 'OrderController',
+      controllerAs: 'oc'
+    })
+    .otherwise({
+      redirectTo: 'home'
+    })
+}]);

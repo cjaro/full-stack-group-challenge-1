edd684d0cbf1b7f8407c99959fdabf3f5d2b00f2
Paige Bystrom <pdbystrom@gmail.com>
2017-03-09 13:24:37 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = expres();
+var bodyParser = require('body-parser');
+
+// APP.USES
+app.use(express.static(path.join(__dirname, './public')));
+
+// ADD APP.USES HERE
+
+
+// APP.LISTEN
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+    console.log('Listening on port: ', app.get('port'));
+});

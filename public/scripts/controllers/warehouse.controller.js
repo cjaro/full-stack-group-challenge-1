332b2fd4e771ebb61fd57a5ad2977f89acfef5de
samvelie <sam.velie@gmail.com>
2017-03-09 14:10:28 -0600

    small changes

---
+++ public/scripts/controllers/warehouse.controller.js
@@ -0,0 +1,16 @@
+myApp.controller('WarehouseController', ['WarehouseFactory', function(WarehouseFactory){
+  var self=this;
+
+  self.dummyData = [
+    {
+    warehouse: alpha,
+    days: 3
+  },
+  {
+    warehouse: beta,
+    days: 4
+  }
+]
+
+
+}])

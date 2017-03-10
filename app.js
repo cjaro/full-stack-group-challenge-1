2b52c15e4cb2abd6f490a9f93afaf11c058ddec6
Paige Bystrom <pdbystrom@gmail.com>
2017-03-10 09:54:35 -0600

    changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var warehouse = require('warehouse');
 
 // Handle index file separately
 app.get('/', function(req, res) {
@@ -15,7 +16,7 @@ app.use(express.static(path.join(__dirname, './public')));
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use('/warehouse', warehouse)
 
 // APP.LISTEN
 app.set('port', process.env.PORT || 5000);

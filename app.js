46688c5b2c97ef1cf6bf1300c39fa2e9fc254909
Paige Bystrom <pdbystrom@gmail.com>
2017-03-10 10:03:37 -0600

    removed example mistake

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-var warehouse = require('warehouse');
 
 // Handle index file separately
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'));
-})
+});
 
 // APP.USES
 app.use(express.static(path.join(__dirname, './public')));
@@ -16,7 +15,6 @@ app.use(express.static(path.join(__dirname, './public')));
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/warehouse', warehouse)
 
 // APP.LISTEN
 app.set('port', process.env.PORT || 5000);

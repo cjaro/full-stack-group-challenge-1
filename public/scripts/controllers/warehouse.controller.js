601c7a185eebf5c2ce724ee72f0dd281643f9552
samvelie <sam.velie@gmail.com>
2017-03-10 16:32:41 -0600

    search on warehouses but not uniquely ordering

--- public/scripts/controllers/warehouse.controller.js
+++ public/scripts/controllers/warehouse.controller.js
@@ -5,5 +5,20 @@ myApp.controller('WarehouseController', ['WarehouseFactory', function(WarehouseF
   WarehouseFactory.getWarehouses();
   self.warehousesObject = WarehouseFactory.warehousesObject;
 
+  console.log('warehousesObject.list', self.warehousesObject.list);
+
+  console.log('warehousesObject length', self.warehousesObject.list.length);
+
+
+
+// forIterate();
+
+// function forIterate() {
+  // WarehouseFactory.getWarehouses().then(function(response) {
+
+
+
+  // });
+
 
 }]);
